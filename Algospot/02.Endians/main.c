a90a9b471469c95d8d20ee29a2e95a795e0d08eb
whatsup0323 <whatsup0323@naver.com>
2018-11-17 21:12:43 +0000

    Edians

---
+++ Algospot/02.Endians/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+	int testNum;
+	unsigned int input = 0, result = 0;
+	unsigned int convertFunc(unsigned long);
+
+	scanf("%d", &testNum);
+	for (int i = 0; i < testNum; i++) {
+		scanf("%u", &input);
+		result = convertFunc(input);
+		printf("%u\n", result);
+	}
+	return 0;
+}
+
+unsigned int convertFunc(unsigned int input) {
+	unsigned int result = 0;
+	unsigned char temp[4] = { 0, };
+	
+	for (int i = 0; i < 4; i++) {
+		temp[i] = input >> (i*8);
+	}
+	result = temp[0] << 24 | temp[1] << 16 | temp[2] << 8 | temp[3];
+
+	return result;
+}
